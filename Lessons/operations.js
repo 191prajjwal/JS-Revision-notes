50d78ff21eadf4a9a35d14ac5ec31a192f72abc1
Prajjwal Shukla <191sprajjwal@gmail.com>
2024-08-25 06:17:32 +0000

    day0.6

---
+++ Lessons/operations.js
@@ -0,0 +1,22 @@
+let value=3
+let negValue=-value
+console.log(negValue);
+
+
+console.log(2*3)//multiplication operator
+console.log(2**3)//power operator
+console.log(20%3)//modulous operator or remainder operator
+
+
+console.log(+true)
+console.log(+false);
+console.log(+'');
+
+
+let a=10
+let b=++a; //increment a then store
+console.log(b)
+
+let x=10
+let y=x++;//store current value to y then increment x
+console.log(y)
