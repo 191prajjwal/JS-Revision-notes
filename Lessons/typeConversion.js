e8e88d5f746e8dd65a160a40cd53a1aeb72f2a35
Prajjwal Shukla <191sprajjwal@gmail.com>
2024-08-25 05:49:27 +0000

    day0.5

---
+++ Lessons/typeConversion.js
@@ -0,0 +1,54 @@
+let score="33"
+console.log(score);
+console.log(typeof score )
+
+let valueInNumber= Number(score)
+console.log("\n"+valueInNumber);
+console.log(typeof valueInNumber);
+
+
+let str="33a"
+console.log("\n"+str)
+console.log(typeof str)
+
+let strInNumber=Number(str)
+console.log("\n"+strInNumber);
+console.log(typeof strInNumber);
+//if we try converting a string that has other characters than numbers then it will be Not a Number NaN
+
+
+
+// let a=null
+// let b=Boolean(a)
+// console.log(b)
+
+
+
+// let a=null
+// let b=Number(a)
+// console.log("\n"+b)
+
+
+// let a=undefined
+// let b=Number(a)
+// console.log("\n"+b)
+
+
+
+// let a=Symbol("hi")
+// let b=Number(a)
+// console.log("\n"+b)
+
+
+// let a=false
+// let b=Symbol(a)
+// console.log("\n"+b)
+
+
+// let a=''
+// let b=Boolean(a)
+// console.log("\n"+b)
+
+// let a="false"
+// let b=Boolean(a)
+// console.log("\n"+b)
