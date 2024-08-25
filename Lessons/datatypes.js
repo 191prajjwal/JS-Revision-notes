36785866ff12090402cd7aef36612a6b69142598
Prajjwal Shukla <191sprajjwal@gmail.com>
2024-08-25 05:16:50 +0000

    day0.4

---
+++ Lessons/datatypes.js
@@ -0,0 +1,24 @@
+//Primitive data types
+
+
+let num1=1
+let num2=2.5
+let num3=122222222222222223445
+let num4=122222222222222223445n
+let str="yes boss"
+let string='ok boss'
+let a= undefined;
+let b;
+let c=null;
+let d=true;
+const mySymbol=Symbol("hi")
+
+
+console.table([num1,num2,num3,num4,str,string,a,b,c,d,mySymbol])
+console.table([typeof(num1),typeof(num2),typeof(num3),typeof(num4),typeof(str),typeof(string),typeof(a),typeof(b),typeof(c),typeof(d),typeof(mySymbol)]);
+
+
+
+
+//Non primitive data types
+
